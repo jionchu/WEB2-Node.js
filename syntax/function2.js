52b071a1b8a7a163a5f348569a4f056e7c791ebb
jionchu <fn0809@gmail.com>
2020-01-02 23:46:58 +0900

    function

---
+++ syntax/function2.js
@@ -0,0 +1,10 @@
+// round: 반올림 해주는 함수
+console.log(Math.round(1.6)); //2
+console.log(Math.round(1.4)); //1
+
+function sum(first, second){ // parameter
+    // return을 만나면 즉시 함수 종료
+    return first+second;
+}
+
+console.log(sum(2,4)); // 각각의 숫자: argument
